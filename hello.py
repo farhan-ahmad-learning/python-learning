a367bdf48cb4e5ee47fd755780280be4e8689a92
farhan-ahmad-learning <farhansa143@gmail.com>
2026-07-03 20:12:56 +0530

    Add files via upload
    week1 - all Python files Monday to Thursday

---
+++ hello.py
@@ -0,0 +1,18 @@
+# My first Python script
+# Day 1 - Week 1
+
+name = input ("What is your name? ")
+city = input ("Which city are you from? ")
+language = input ("What language do you speak? ")
+
+print(f"Hello {name}!")
+print(f"You are from {city}.")
+print(f"You speak {language}.")
+print(f"Welcome to Python programming!")
+
+age = input ("How old are you? ")
+print(f"In 10 years you will be {int(age) + 10} year old.")
+
+print(f"Your name has {len(name)} letters.")
+
+print(f"Your name in uppercase : {name.upper()}")
---
+++ profile.py
@@ -0,0 +1,12 @@
+name = input ("What is your name? ")
+city = input ("Which city do you live in? ")
+title = input ("What is your Job Title? ")
+Exp = input ("How much experience you have in years? ")
+
+print(f"======== PROFESSIONAL PROFILE ========")
+print(f"NAME        :   {name.upper()}")
+print(f"City        :   {city}")
+print(f"Job Title   :   {title}")
+print(f"Experience  :   {Exp} years")
+print(f"By 2030     :   {int(Exp) + 5} years of experience")
+print(f"Name length :   {len(name)} characters")
